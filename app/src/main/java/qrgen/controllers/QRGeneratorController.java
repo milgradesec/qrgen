f424ccafec2466edf1bcf0b3c49452662caa07db
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-04-25 16:13:56 +0200

    Create QRGeneratorController.java

---
+++ app/src/main/java/qrgen/controllers/QRGeneratorController.java
@@ -0,0 +1,65 @@
+package qrgen.controllers;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.WriterException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import qrgen.QRGenerator;
+import qrgen.models.RequestModel;
+
+@RestController
+public class QRGeneratorController {
+
+    private static final String DEFAULT_QR_VALUE = "https://blog.paesa.es/";
+
+    @RequestMapping("/")
+    public RedirectView homeHandler() {
+        RedirectView rv = new RedirectView("/qr");
+        rv.setStatusCode(HttpStatus.FOUND);
+        return rv;
+    }
+
+    @GetMapping("/qr")
+    public ResponseEntity<byte[]> generateFromVar(
+            @RequestParam(value = "data", defaultValue = DEFAULT_QR_VALUE) final String data)
+            throws IOException, WriterException {
+
+        final BufferedImage bimg = QRGenerator.createQRImage(data, 400);
+        final byte[] bytes;
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(bimg, "jpg", baos);
+            bytes = baos.toByteArray();
+        }
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
+    }
+
+    @PostMapping(path = "/qr", consumes = "application/json", produces = "image/jpeg")
+    public ResponseEntity<byte[]> generateFromJSON(@RequestBody final RequestModel request)
+            throws WriterException, IOException {
+
+        final BufferedImage bimg = QRGenerator.createQRImage(request.getData(), 400);
+        final byte[] bytes;
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(bimg, "jpg", baos);
+            bytes = baos.toByteArray();
+        }
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
+    }
+}
