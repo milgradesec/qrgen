15d8dcc6ee92ac5e4f691cc68be01998e70783dd
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-09-18 17:57:54 +0200

    Remove POST handler

--- server/src/main/java/com/github/milgradesec/qrgen/controller/QRController.java
+++ server/src/main/java/com/github/milgradesec/qrgen/controller/QRController.java
@@ -3,14 +3,11 @@ package com.github.milgradesec.qrgen.controller;
 import java.io.IOException;
 
 import com.github.milgradesec.qrgen.QRGenerator;
-import com.github.milgradesec.qrgen.model.Request;
 import com.google.zxing.WriterException;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +19,8 @@ public class QRController {
     private static final String DEFAULT_IMAGE_FORMAT = "jpg";
 
     @GetMapping("/qr")
-    public ResponseEntity<byte[]> getFromVar(@RequestParam(name = "data", defaultValue = DEFAULT_QR_VALUE) String data,
+    public ResponseEntity<byte[]> getFromVar(
+            @RequestParam(name = "data", defaultValue = DEFAULT_QR_VALUE) final String data,
             @RequestParam(name = "size", required = false) Integer size) throws IOException, WriterException {
 
         if (size == null) {
@@ -30,12 +28,5 @@ public class QRController {
         }
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG)
                 .body(QRGenerator.generateFromString(data, size, DEFAULT_IMAGE_FORMAT));
-    }
-
-    @PostMapping(path = "/qr", consumes = "application/json", produces = "image/jpeg")
-    public ResponseEntity<byte[]> getFromJSON(@RequestBody Request request) throws WriterException, IOException {
-
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG)
-                .body(QRGenerator.generateFromString(request.getData(), DEFAULT_IMAGE_SIZE, DEFAULT_IMAGE_FORMAT));
     }
 }
