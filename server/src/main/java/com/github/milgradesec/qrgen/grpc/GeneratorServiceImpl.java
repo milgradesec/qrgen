05e9fce207467c95cec23df8e2848ee6e4908401
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-09-24 22:22:26 +0200

    Update GeneratorServiceImpl.java

--- server/src/main/java/com/github/milgradesec/qrgen/grpc/GeneratorServiceImpl.java
+++ server/src/main/java/com/github/milgradesec/qrgen/grpc/GeneratorServiceImpl.java
@@ -5,8 +5,8 @@ import io.grpc.stub.StreamObserver;
 public class GeneratorServiceImpl extends GeneratorServiceGrpc.GeneratorServiceImplBase {
     @Override
     public void generate(QRRequest req, StreamObserver<QRResponse> responseObserver) {
-        QRResponse reply = QRResponse.newBuilder().setStatus("OK").build();
-        responseObserver.onNext(reply);
+        QRResponse resp = QRResponse.newBuilder().setStatus("OK").build();
+        responseObserver.onNext(resp);
         responseObserver.onCompleted();
     }
 }
